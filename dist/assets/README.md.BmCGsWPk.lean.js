9930276a570711845b73798b940b4b7005ba540c
李 文博 <2731909292@qq.com>
2024-06-03 23:17:37 +0800

    删除多余文件

--- dist/assets/README.md.BmCGsWPk.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a,a4 as o}from"./chunks/framework.BMnk781U.js";const g=JSON.parse('{"title":"My_blog","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md"}'),r={name:"README.md"},i=o("",46),s=[i];function n(l,h,d,c,u,p){return a(),t("div",null,s)}const b=e(r,[["render",n]]);export{g as __pageData,b as default};
